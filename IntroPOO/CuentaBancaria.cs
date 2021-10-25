7a3b18f9b1322e7d85178c3773b45dde1b745b6b
dwn84 <dwn84@hotmail.com>
2021-10-25 18:53:13 -0500

    Propiedades, valores por defecto

--- IntroPOO/CuentaBancaria.cs
+++ IntroPOO/CuentaBancaria.cs
@@ -10,20 +10,37 @@ namespace IntroPOO
         private string nombreTitular;
         private int numeroCuenta;
         private double saldo;
+        private string fechaCreacion;
+        private string _sucursal;
+        public bool vip;//No cumple con la abstracción - No recomendable dejar el campo publico
+        //Propiedades de clase
+        public string NombreTitular {get => nombreTitular;set => nombreTitular = value;}
+        public int NumeroCuenta { get => numeroCuenta; set => numeroCuenta = value; }
+        public double Saldo { get => saldo; set => saldo = value; }
+        public string FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+
 
         //Constructor de clase
         public CuentaBancaria(string nt, int nc, double s,bool xyz) {
-            nombreTitular = nt;
-            numeroCuenta = nc;
-            saldo = s;
+            NombreTitular = nt;
+            NumeroCuenta = nc;
+            Saldo = s;
         }
         //segundo constructor de clase: sobrecarga del constructor
         public CuentaBancaria(string nn) {
-            nombreTitular = nn;
+            NombreTitular = nn;
         }
         //tercer constructo de clase
         public CuentaBancaria(int saldito) {
-            saldo = saldito;
+            Saldo = saldito;
+        }
+
+        public CuentaBancaria() {
+        
+        }
+
+        public void CambiarSucursalCliente(string nuevaSucursal, string fechaCambio = "1999/01/01") {
+            Console.WriteLine("Se ha cambiado la sucursal: " + fechaCambio);
         }
 
         //Comportamiento, acciones o métodos: funciones o procedimientos
@@ -31,9 +48,10 @@ namespace IntroPOO
         //Los métodos alteran o muestras los campos de la clase
         public void Retirar(double dineroPorRetirar)
         {
-            if (dineroPorRetirar < saldo)
+           
+            if (dineroPorRetirar < Saldo)
             {
-                saldo = saldo - dineroPorRetirar;
+                Saldo = Saldo - dineroPorRetirar;
                 Console.WriteLine("Retirando dinero!");
             }
             else
@@ -46,14 +64,14 @@ namespace IntroPOO
         
         public void Consignar(double dineroPorDepositar, string fecha, string sucursal, string nombreConsignador)
         {
-            saldo = saldo + dineroPorDepositar;
+            Saldo = Saldo + dineroPorDepositar;
             Console.WriteLine("El día {0} se ha realizado una consignación de: " +
                               "{1} que se llevó a cabo en la sucursual: " +
                               "{2} por la persona:{3}", fecha, dineroPorDepositar, sucursal, nombreConsignador);
         }
 
         public void MostrarSaldoTotal() {
-            Console.WriteLine("El saldo total disponible es:{0}",saldo);
+            Console.WriteLine("El saldo total disponible es:{0}",Saldo);
         }
     }
 }
--- IntroPOO/Program.cs
+++ IntroPOO/Program.cs
@@ -17,15 +17,17 @@ namespace IntroPOO
             puntico.TrasladarCoordenadaY();
             Console.WriteLine("Coordenada Y:{0}", puntico.CoordenadaY);
             Console.WriteLine("Objeto creado con una clase sin constructor:");
-            CuentaBancaria colmena = new CuentaBancaria("ggf",55,22,true);
+            CuentaBancaria colmena = new CuentaBancaria();
+            colmena.CambiarSucursalCliente("Laureles");
+            Console.WriteLine(colmena.Saldo);
             colmena.MostrarSaldoTotal();
 
             CuentaBancaria banpolombia = new CuentaBancaria("wtf");
             CuentaBancaria davivienda = new CuentaBancaria("Oelo");
             CuentaBancaria conavi = new CuentaBancaria(54635364);
-            banpolombia.saldo = 100000;
-            banpolombia.nombreTitular = "Nerón Navarrete";
-            banpolombia.numeroCuenta = 111221;
+            banpolombia.Saldo = 100000;
+            banpolombia.NombreTitular = "Nerón Navarrete";
+            banpolombia.NumeroCuenta = 111221;
             banpolombia.Retirar(10000);
             banpolombia.MostrarSaldoTotal();
             banpolombia.Retirar(20000);
@@ -39,10 +41,10 @@ namespace IntroPOO
             banpolombia.Retirar(3000);
 
 
-            davivienda.saldo = 33000;
-            davivienda.nombreTitular = "Esperanza Gómez";
-            davivienda.numeroCuenta = 111223;
-            davivienda.saldo = 333333333;
+            davivienda.Saldo = 33000;
+            davivienda.NombreTitular = "Esperanza Gómez";
+            davivienda.NumeroCuenta = 111223;
+            davivienda.Saldo = 333333333;
             davivienda.Retirar(11111111);
             davivienda.Consignar(10000000,"14/10/2021","Edicio Coltejer","Edwin");
             davivienda.Consignar(2222222, "14/10/2021","Centro Comercial Premium","Esperzanza");
